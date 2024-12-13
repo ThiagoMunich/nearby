3d2b51ce1b7c78a41a845086376c7743c3877068
Thiago Munich <thiago.munich@direcional.com.br>
2024-12-12 22:24:27 -0300

    feat: insert animation on step component

--- src/components/step/index.tsx
+++ src/components/step/index.tsx
@@ -1,25 +1,23 @@
 import { View, Text, ViewProps } from "react-native"
 
-import { IconProps } from "@tabler/icons-react-native"
-
 import { s } from "./styles"
-import { colors } from "@/styles/theme"
+import Animated, { FadeInDown, FadeInUp, LinearTransition, SlideInLeft, SlideInRight } from "react-native-reanimated"
 
 interface StepProps extends ViewProps {
   title: string
   description: string
   icon?: any
+  index: number
 }
-export function Step({ title, description, icon, ...rest }: StepProps) {
+export function Step({ title, description, icon, index, ...rest }: StepProps) {
   return (
-    <View {...rest} style={s.container}>
-      {/* {Icon && <Icon size={32} color={colors.green.base} />} */}
-      {icon}
+    <Animated.View entering={FadeInUp.delay((index + 1) * 1500).duration(1000)} {...rest} style={s.container}>
+      <View>{icon}</View>
       <View style={s.details}>
         <Text style={s.title}>{title}</Text>
 
         <Text style={s.description}>{description}</Text>
       </View>
-    </View>
+    </Animated.View>
   )
 }
--- src/components/steps/index.tsx
+++ src/components/steps/index.tsx
@@ -5,6 +5,7 @@ import { Ionicons, Entypo, MaterialCommunityIcons } from "@expo/vector-icons"
 import { s } from "./styles"
 import { Step } from "../step"
 import { colors } from "@/styles/theme"
+import Animated, { FadeInUp } from "react-native-reanimated"
 
 const iconConfig = {
   size: 32,
@@ -32,10 +33,14 @@ const steps = [
 export function Steps() {
   return (
     <View style={s.container}>
-      <Text style={s.title}>Veja como funciona:</Text>
+      <Animated.Text entering={FadeInUp.duration(600).delay(1000)} style={s.title}>
+        Veja como funciona:
+      </Animated.Text>
 
-      {steps.map((step) => {
-        return <Step key={step.title} icon={step.icon} title={step.title} description={step.description} />
+      {steps.map((step, index) => {
+        return (
+          <Step key={step.title} index={index} icon={step.icon} title={step.title} description={step.description} />
+        )
       })}
     </View>
   )
