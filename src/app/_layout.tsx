008aacc003418915f8e404e0fb789b781eeef8ca
Thiago Munich <thiago.munich@direcional.com.br>
2024-12-17 14:43:40 -0300

    feat: insert map on home route

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,12 @@
 import { Stack } from "expo-router"
 
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+
 import { colors } from "@/styles/theme"
 
 import { useFonts, Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold } from "@expo-google-fonts/rubik"
 
 import { Loading } from "@/components/loading"
-import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 export default function Layout() {
   const [isFontsLoaded] = useFonts({
--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,6 +1,8 @@
-import { useCallback, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 import { View } from "react-native"
+
+import MapView from "react-native-maps"
 
 import { Categories } from "@/components/categories"
 import { useFetchPlaces } from "@/hooks/useFetchPlaces"
@@ -13,6 +15,11 @@ export default function Home() {
   const { categories, fetchCategories } = useFetchCategories()
 
   const [selectedCategory, setSelectedCategory] = useState("")
+
+  const initialLocation = {
+    latitude: -23.561187293883442,
+    longitute: -46.656451388116494,
+  }
 
   useEffect(() => {
     if (categories.length !== 0) {
@@ -29,6 +36,16 @@ export default function Home() {
   return (
     <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
       <Categories data={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory} />
+
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: initialLocation.latitude,
+          longitude: initialLocation.longitute,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      />
 
       <Places data={places} />
     </View>
